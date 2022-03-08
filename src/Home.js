6b24293680499c50e1addd14f76838a20381d858
sade-online <fot.fasan@gmail.com>
2022-03-08 00:01:30 +0000

    css styles and event handling

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log("Hello, you!");
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click mee!</button>
+            {/* annonymous function for user input to prevent automatic invokation*/}
+            <button></button>
         </div>
      );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -3,8 +3,12 @@ const Navbar = () => {
         <nav className="navbar">
             <h1>The Mojo Blog</h1>
             <div className="links">
-                <a href="/">Home</a> <br />
-                <a href="/create">Create New Blog</a>
+                <a href="/">Home</a>
+                <a href="/create" style={{
+                    color:"white",
+                    backgroundColor:"#e07a5f",
+                    borderRadius: '8px'
+                }} >Create New Blog</a>
 
                 
             </div>
