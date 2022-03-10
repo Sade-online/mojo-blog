77b1fc9dab0424b164bb3cbeef7fdaa0c91f77b3
sade-online <fot.fasan@gmail.com>
2022-03-10 23:28:36 +0000

    annonymous function and events object

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,19 @@
 const Home = () => {
 
-    const handleClick = () => {
-        console.log("Hello, you!");
+    const handleClick = (e) => {
+        console.log("Hello, you!", e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('Hello ' + name, e.target);
     }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
             <button onClick={handleClick}>Click mee!</button>
-            {/* annonymous function for user input to prevent automatic invokation*/}
-            <button></button>
+            {/* annonymous function for user input to prevent automatic invokation below*/}
+            <button onClick= {(e) => handleClickAgain('Yoshi', e)}>Click me again!</button>
         </div>
      );
 }
