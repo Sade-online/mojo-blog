07b993c42912a009b48831fa51e83f2ba2d1bdf9
sade-online <fot.fasan@gmail.com>
2022-03-13 21:37:00 +0000

    lists aka arrays in REACT

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,25 @@
 import { useState } from "react";
 
 const Home = () => {
-  // let name = 'pickachu';
-  // useState is a function. give it the initial value of pickachu
-  const [name, setName] = useState("Pickachu");
-
-  const [age, setAge] = useState(21);
-
-  const handleClick = () => {
-    setName("Garchomp");
-    setAge(25);
-  };
-
+  //lists aka arrays to be cycled through regardless of changes using map method - return a template.
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>
-        {name} is {age} years old
-      </p>
-      <button onClick={handleClick}>Click mee!</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
